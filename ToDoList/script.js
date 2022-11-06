223009f38e7e99cda023f1a5a22f5a8d9c32c022
jmorian <117075264+ejmorian@users.noreply.github.com>
2022-11-06 17:02:44 +0800

    Add localStorage and Refactor utilities
    Changes are now save to local storage. edit, add, checkbox functions are updated.

--- ToDoList/script.js
+++ ToDoList/script.js
@@ -1,71 +1,75 @@
+// Get DOM objects
 const inputform = document.querySelector('.inputcontainer');
 const taskinput = document.querySelector('.taskinput');
 const addbutton = document.querySelector('.addbutton');
-const taskbox = document.querySelector('.taskbox')
+const taskbox = document.querySelector('.taskbox');
 
-inputform.addEventListener('submit', e =>{
-    e.preventDefault();
-})
+// LOAD Existing task
+window.addEventListener('load', (event) => {
+    taskbox.innerHTML = localStorage.getItem('taskbox')
 
-function createTask() {
-    const taskcontainer = document.createElement('div')
-    taskcontainer.classList.add('taskbox__element');
+    const taskcontainer = document.querySelectorAll('.taskbox__element')
+    const checkbox = document.querySelectorAll('.checkbox')
+    const taskoutput = document.querySelectorAll('.taskoutput');
+    const editbutton = document.querySelectorAll('.editbutton');
+    const deletebutton = document.querySelectorAll('.deletebutton');
 
-    const checkbox = document.createElement('input')
-    checkbox.setAttribute('type', 'checkbox')
-    checkbox.classList.add('checkbox')
+    const empty = document.createElement('input')
+    empty.setAttribute('type', 'text');
 
-    const taskoutput = document.createElement('input');
-    taskoutput.classList.add('taskoutput')
-    taskoutput.setAttribute('type', 'text');
-    taskoutput.readOnly = true;
-    taskoutput.value = taskinput.value;
+    for (i = 0; i < taskcontainer.length; i++) {
 
-    const editbutton = document.createElement('span');
-    editbutton.classList.add('editbutton')
-    editbutton.textContent = 'EDIT'
+        let x = i;
 
-    const deletebutton = document.createElement('span')
-    deletebutton.classList.add('deletebutton')
-    deletebutton.textContent = 'DELETE'
+        deletebutton[i].addEventListener('click', () => {
+            console.log('deletebutton clicks')
+            taskcontainer[x].remove();
+            save()
 
-    editbutton.addEventListener('click', () =>{
-        if (taskoutput.value !== null && taskoutput.value !== ''){
-            if (taskoutput.readOnly)
-            {
-                taskoutput.readOnly = false;
-                editbutton.textContent = 'SAVE';
-            }else if(editbutton.textContent === 'SAVE')
-            {
-            editbutton.textContent = 'EDIT'
-            taskoutput.readOnly = true;
+        })
+
+        editbutton[i].addEventListener('click', () => {
+            console.log('editbutton clicks')
+
+            if (taskoutput[x].readOnly) {
+                taskoutput[x].readOnly = false;
+                const end = taskoutput[x].value.length;
+                taskoutput[x].setSelectionRange(end, end);
+                taskoutput[x].focus();
+                editbutton[x].textContent = 'SAVE';
+                save()
+            } else {
+                empty.value = taskoutput[x].value;
+                taskoutput[x].readOnly = true;
+                editbutton[x].textContent = 'EDIT';
+                taskoutput[x].setAttribute('value', empty.value);
+                save()
+                console.log(taskoutput[x].value);
             }
-        } else {
-            alert('please enter a task')
-        }
+            save()
+        })
+        checkbox[i].addEventListener('click', () => {
+            console.log('checkbox clicks')
+            if (checkbox[x].checked == true) {
+                checkbox[x].setAttribute('checked', true);
+                taskoutput[x].style.textDecoration = 'line-through'
+                save()
+            } else if (checkbox[x].checked == false) {
+                checkbox[x].removeAttribute('checked');
+                taskoutput[x].style.textDecoration = 'none'
+                save()
+            }
+        })
+    }
+
+});
 
 
-    })
+// addTask utility
 
-    checkbox.addEventListener('click', ()=>{
-        if (checkbox.checked == true){
-            taskoutput.style.textDecoration = 'line-through'    
-        } else{
-            taskoutput.style.textDecoration = 'none'   
-        }
-    })
-
-    deletebutton.addEventListener('click', ()=>{
-        taskcontainer.remove();
-    })
-
-    taskcontainer.append(checkbox,taskoutput,editbutton,deletebutton);
-    taskbox.append(taskcontainer);
-}
-
-taskinput.addEventListener('keypress', e =>{
+taskinput.addEventListener('keypress', e => {
     if (taskinput.value !== null && taskinput.value !== '') {
-        if(e.key == 'Enter'){
+        if (e.key == 'Enter') {
             createTask()
             taskinput.value = '';
             console.log('this is working')
@@ -73,7 +77,7 @@ taskinput.addEventListener('keypress', e =>{
     }
 })
 
-addbutton.addEventListener('click', ()=>{
+addbutton.addEventListener('click', () => {
     if (taskinput.value !== null && taskinput.value !== '') {
         createTask()
         taskinput.value = '';
@@ -81,4 +85,79 @@ addbutton.addEventListener('click', ()=>{
     }
 })
 
+inputform.addEventListener('submit', e => {
+    e.preventDefault();
+})
+// --------
+
+function createTask() {
+
+    const taskcontainer = document.createElement('div')
+    const checkbox = document.createElement('input')
+    const taskoutput = document.createElement('input');
+    const editbutton = document.createElement('span');
+    const deletebutton = document.createElement('span')
+    const empty = document.createElement('input')
+
+    empty.setAttribute('type', 'text');
+    taskcontainer.classList.add('taskbox__element');
+    checkbox.setAttribute('type', 'checkbox')
+    checkbox.classList.add('checkbox')
+    taskoutput.classList.add('taskoutput')
+    taskoutput.setAttribute('type', 'text');
+    taskoutput.readOnly = true;
+    editbutton.classList.add('editbutton')
+    editbutton.textContent = 'EDIT'
+    deletebutton.classList.add('deletebutton')
+    deletebutton.textContent = 'DELETE'
+
+    editbutton.addEventListener('click', () => {
+
+        if (taskoutput.value !== null && taskoutput.value !== '') {
+            if (taskoutput.readOnly) {
+                const end = taskoutput.value.length;
+                taskoutput.setSelectionRange(end, end);
+                taskoutput.focus();
+                taskoutput.readOnly = false;
+                editbutton.textContent = 'SAVE';
+            } else if (editbutton.textContent === 'SAVE') {
+                empty.value = taskoutput.value;
+                taskoutput.setAttribute('value', empty.value)
+                editbutton.textContent = 'EDIT'
+                taskoutput.readOnly = true;
+            }
+        } else {
+            alert('please enter a task')
+        }
+        save()
+    })
+
+    checkbox.addEventListener('click', () => {
+        if (checkbox.checked == true) {
+            checkbox.setAttribute('checked', true);
+            taskoutput.style.textDecoration = 'line-through'
+            save()
+        } else if (checkbox.checked == false) {
+            checkbox.removeAttribute('checked');
+            taskoutput.style.textDecoration = 'none'
+            save()
+        }
+    })
+
+    deletebutton.addEventListener('click', () => {
+        taskcontainer.remove();
+        save()
+    })
+    taskoutput.setAttribute('value', taskinput.value);
+    taskcontainer.append(checkbox, taskoutput, editbutton, deletebutton);
+    taskbox.append(taskcontainer);
+
+    save()
+
+}
+
+// SAVE changes to task 
+function save() {
+    window.localStorage.setItem('taskbox', taskbox.innerHTML);
+}
 

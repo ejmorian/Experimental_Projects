8ef147f0e2fe09c329037c0060e5da5bfef98e05
jmorian <117075264+ejmorian@users.noreply.github.com>
2022-11-15 13:40:36 +0800

    Add Interval and Clean Up Code

--- TripCountdownTimer/script.js
+++ TripCountdownTimer/script.js
@@ -1,24 +1,19 @@
-const now = new Date();
-const departureDay = new Date(2023,1,7)
-const timeDifference = departureDay.getTime() - now.getTime();
+function countDown() {
+  const now = new Date();
+  const departureDay = new Date(2023, 1, 7)
 
-const toSeconds = timeDifference / 1000
-const toMinutes = toSeconds / 60
-const toHours = toMinutes /60
-const toDays = toHours / 24
-const toWeeks = toDays / 7
+  const timeDifference = departureDay.getTime() - now.getTime();
 
-const weeksLeft = timeDifference / 604800000;
-const daysLeft = timeDifference / 86400000;
-const hoursLeft = timeDifference / 3600000;
-const minutesLeft = timeDifference / 60000;
-const secondsLeft = timeDifference / 1000;
+  const weeks = timeDifference % 604800000;
+  const days = (timeDifference % 604800000) / 86400000;
+  const hours = ((timeDifference % 604800000) % 86400000) / 3600000;
+  const minutes = (((timeDifference % 604800000) % 86400000) % 3600000) / 60000;
+  const seconds = ((((timeDifference % 604800000) % 86400000) % 3600000) % 60000) / 1000;
 
-const weeks = weeksLeft;
-const days = (timeDifference % 604800000)/86400000;
-const hours = ((timeDifference % 604800000)%86400000)/3600000;
-const minutes = (((timeDifference % 604800000)%86400000)%3600000)/60000;
-const seconds = ((((timeDifference % 604800000)%86400000)%3600000)%60000)/1000;
+  console.log(Math.floor(weeks) + " weeks " + Math.floor(days) + "  days " + Math.floor(hours) + " hours " + Math.floor(minutes) + " minutes " + Math.floor(seconds) + " seconds ");
 
+  console.log(timeDifference);
 
-console.log(seconds);
+}
+
+setInterval(countDown, 1000);

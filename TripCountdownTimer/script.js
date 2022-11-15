29d2c27f5cf5323c9d9445401a9db73ca6dff329
jmorian <117075264+ejmorian@users.noreply.github.com>
2022-11-15 16:21:30 +0800

    Inject Countdown Timer to Document

--- TripCountdownTimer/script.js
+++ TripCountdownTimer/script.js
@@ -1,19 +1,33 @@
+const weeksAmount = document.querySelector('.weeksAmount');
+const daysAmount = document.querySelector('.daysAmount');
+const hoursAmount = document.querySelector('.hoursAmount');
+const minutesAmount = document.querySelector('.minutesAmount');
+const secondsAmount = document.querySelector('.secondsAmount');
+
+
+
 function countDown() {
-  const now = new Date();
-  const departureDay = new Date(2023, 1, 7)
+    const now = new Date();
+    const departureDay = new Date(2023, 1, 7)
+  
+    const timeDifference = departureDay.getTime() - now.getTime();
+  
+    const weeks = timeDifference / 604800000;
+    const days = (timeDifference % 604800000) / 86400000;
+    const hours = ((timeDifference % 604800000) % 86400000) / 3600000;
+    const minutes = (((timeDifference % 604800000) % 86400000) % 3600000) / 60000;
+    const seconds = ((((timeDifference % 604800000) % 86400000) % 3600000) % 60000) / 1000;
 
-  const timeDifference = departureDay.getTime() - now.getTime();
-
-  const weeks = timeDifference % 604800000;
-  const days = (timeDifference % 604800000) / 86400000;
-  const hours = ((timeDifference % 604800000) % 86400000) / 3600000;
-  const minutes = (((timeDifference % 604800000) % 86400000) % 3600000) / 60000;
-  const seconds = ((((timeDifference % 604800000) % 86400000) % 3600000) % 60000) / 1000;
-
-  console.log(Math.floor(weeks) + " weeks " + Math.floor(days) + "  days " + Math.floor(hours) + " hours " + Math.floor(minutes) + " minutes " + Math.floor(seconds) + " seconds ");
-
-  console.log(timeDifference);
-
-}
-
-setInterval(countDown, 1000);
+    weeksAmount.textContent = Math.floor(weeks);
+    daysAmount.textContent = Math.floor(days);
+    hoursAmount.textContent = Math.floor(hours);
+    minutesAmount.textContent = Math.floor(minutes);
+    secondsAmount.textContent = Math.floor(seconds);
+  
+    console.log(Math.floor(weeks) + " weeks " + Math.floor(days) + "  days " + Math.floor(hours) + " hours " + Math.floor(minutes) + " minutes " + Math.floor(seconds) + " seconds ");
+  
+    console.log(timeDifference);
+  
+  }
+  
+  setInterval(countDown, 1000);
